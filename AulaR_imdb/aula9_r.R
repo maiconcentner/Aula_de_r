f8b04d238a76daecd6f739e4791da5c4005a649a
maiconcentner <maiconcentner@gmail.com>
2023-05-25 13:24:22 -0300

    Terefa realizada, calculo da correlação

--- AulaR_imdb/aula9_r.R
+++ AulaR_imdb/aula9_r.R
@@ -26,6 +26,8 @@ df <- dados %>%
 #? barras
 #? cada barra vem de uma coluna e aparece com uma cor diferente
 
+#? pivot_longer
+
 df_long <- df %>% 
   slice(1:10) %>% #seleciona os 10 primeiros
   tidyr::pivot_longer(2:4, values_to = "Valor", names_to = "Tipo de valor")
@@ -40,3 +42,17 @@ ggplot()+
     axis.text.x = element_text(angle = 45, hjust = 1.0)
   )
   
+
+#? pivot_winder
+
+df_winder <- df_long %>% 
+  tidyr::pivot_wider(names_from = `Tipo de valor`, values_from = "Valor")
+
+#? calculo correlação
+library(reshape2)
+
+correlacao <- df_winder %>% 
+  select(orcamento, receita, receita_eua) %>% 
+  cor()
+
+correlacao

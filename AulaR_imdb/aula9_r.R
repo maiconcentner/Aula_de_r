b283f207c5af5c7937b7225a89d8042c3d79e2ed
maiconcentner <maiconcentner@gmail.com>
2023-05-25 13:09:56 -0300

    Aula 9

---
+++ AulaR_imdb/aula9_r.R
@@ -0,0 +1,42 @@
+#? Carregando as bibliotecas
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(ggplot2)
+
+#? Setando pasta de trabalho
+setwd("D:\\Mestrado\\1Sem_23\\Ferramentas computacionais\\R_pokemon\\AulaR_imdb")
+
+#? Pacote TIDYR
+library(tidyr)
+
+#? Carregando base
+
+dados <- readr::read_rds("imdb.rds")
+
+head(dados)
+names(dados)
+
+#? Carregando parte específicas da base
+
+df <- dados %>% 
+  select(titulo, orcamento, receita, receita_eua)
+
+#? grafico com 10 primeiro filmes
+#? barras
+#? cada barra vem de uma coluna e aparece com uma cor diferente
+
+df_long <- df %>% 
+  slice(1:10) %>% #seleciona os 10 primeiros
+  tidyr::pivot_longer(2:4, values_to = "Valor", names_to = "Tipo de valor")
+
+#? Gerando o gráfico
+
+ggplot()+
+  geom_col(data = df_long, aes(x = titulo, y = Valor, fill = `Tipo de valor`),
+           position = position_dodge2())+
+  theme_bw()+
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1.0)
+  )
+  
